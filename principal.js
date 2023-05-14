b71d20bd1d5199161f11d5b20eef65baaedc8494
GonzaS89 <gnzx89@gmail.com>
2023-05-13 22:19:39 -0300

    probando

--- principal.js
+++ principal.js
@@ -1,13 +1,25 @@
+const body = document.querySelector('body')
 const header = document.querySelector('.header');
 const carousel = document.querySelector('.carousel-cont');
 const tituloInfo = document.getElementById('titulo');
 const servicio = document.querySelectorAll('.servicio');
-const servicio2 = document.querySelectorAll('.servicio2');
+const servicio2 = document.querySelectorAll('.servicio2')
 
 addEventListener('scroll', ()=> {
-    if(header.getBoundingClientRect().bottom < 0) 
-    servicio.classList.add('servicioAnimacion')
-    servicio2.classList.add('ser')
+
+    if(carousel.getBoundingClientRect().top < 0){
+
+    servicio.forEach(element => {
+        element.classList.add('servicioAnimacion')
+        element.classList.add('servicioAnimacion')
+    });
+
+    servicio2.forEach(element => {
+        element.classList.add('servicio2Animacion')
+        element.classList.add('servicio2Animacion')
+    });
+    
+} 
 })
 
 
