c9aa0bed4167d7da75f1c82940fbd0b869ad1e0f
Gonza <gnzx89@gmail.com>
2023-05-24 09:51:12 -0300

    a

--- principal.js
+++ principal.js
@@ -4,11 +4,15 @@ const tituloInfo = document.getElementById('titulo');
 const servicio = document.querySelectorAll('.servicio');
 const servicio2 = document.querySelectorAll('.servicio2');
 
-addEventListener('scroll', ()=> {
-    if(header.getBoundingClientRect().bottom < 0) 
-    servicio.classList.add('servicioAnimacion')
-    servicio2.classList.add('ser')
-})
+// addEventListener('scroll', ()=> {
+//     if(header.getBoundingClientRect().bottom < 0) ;
+//     servicio.classList.add('servicioAnimacion');
+//     servicio2.classList.add('ser');
+// })
+
+
+const galeria = document.querySelector('.galeria');
+const 
 
 
 
