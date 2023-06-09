623049f1dae786ff7e154ab4c9459dbed5140c42
GonzaS89 <gnzx89@gmail.com>
2023-06-08 23:35:20 -0300

    principal

--- typed.js
+++ typed.js
@@ -9,25 +9,25 @@ let typed = new Typed('.textoCar', {
 });
 
 
-let typed2 = new Typed('.footer-txt1', {
-        strings: ['Empresa Florida 2023'],
-        typeSpeed: 50,
-        backSpeed: 25,
-        cursorChar: '.',
-        shuffle: false,
-        smartBackspace: true,
-        loop: false
-});
+// let typed2 = new Typed('.footer-txt1', {
+//         strings: ['Empresa Florida 2023'],
+//         typeSpeed: 50,
+//         backSpeed: 25,
+//         cursorChar: '.',
+//         shuffle: false,
+//         smartBackspace: true,
+//         loop: false
+// });
 
-let typed3 = new Typed('.footer-txt2', {
-        strings: ['Diseño Web : Sinhg Gonzalo'],
-        typeSpeed: 50,
-        backSpeed: 25,
-        cursorChar: '.',
-        shuffle: false,
-        smartBackspace: true,
-        loop: false
-})
+// let typed3 = new Typed('.footer-txt2', {
+//         strings: ['Diseño Web : Sinhg Gonzalo'],
+//         typeSpeed: 50,
+//         backSpeed: 25,
+//         cursorChar: '.',
+//         shuffle: false,
+//         smartBackspace: true,
+//         loop: false
+// })
 
 // var typed3 = new Typed('.footer-txt2', {
 //         strings: ["Diseño Web por Sinhg Gonzalo"],
