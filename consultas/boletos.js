c2fd76866972d7829747313b1abd5045f6bd68a9
Gonza <gnzx89@gmail.com>
2023-11-27 15:58:53 -0300

    ajustando nueva version

---
+++ consultas/boletos.js
@@ -0,0 +1,16 @@
+const codigo_06 = 100;
+const codigo_08 = 130;
+const codigo_10 = 140;
+const codigo_11 = 160;
+const codigo_12 = 170;
+const codigo_13 = 190;
+const codigo_14 = 200;
+const codigo_15 = 210;
+const codigo_16 = 230;
+const codigo_18 = 240;
+const codigo_20 = 270;
+const codigo_21 = 300;
+const codigo_22 = 340;
+const codigo_23 = 360;
+const codigo_24 = 370;
+const codigo_26 = 
