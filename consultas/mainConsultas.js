aa3cab34eace2a7958f358b3cc4c3f7c919dd33f
Gonza <gnzx89@gmail.com>
2024-03-27 09:18:53 -0300

    cambios por semana santa

--- consultas/mainConsultas.js
+++ consultas/mainConsultas.js
@@ -210,7 +210,7 @@ onload = ()=> {
         let tipoDeDia;
 
         const definirDia = (fecha, mes)=> {
-            tipoDeDia = (fecha == 12 || fecha == 13 && mes == 1) ? 'feriado' : 'normal';
+            tipoDeDia = (dia == 1 || dia == 2 && mes == 3) ? 'feriado' : 'normal';
             switch (tipoDeDia) {
                 case 'feriado':
                     tituloResultado.textContent = `Hoy, ${diasDeLaSemana[dia].toLowerCase()} (feriado): Circulación como día domingo `;
@@ -225,6 +225,15 @@ onload = ()=> {
         }
 
         definirDia (fecha, mes)
+
+        if(dia == 28 && mes == 2) {
+        rutaObtenida = obtenerDiaRuta(6);
+        tituloResultado.textContent = `Hoy, ${diasDeLaSemana[dia].toLowerCase()} santo : Circulación como dia sábado`};
+
+        if(dia == 29 && mes == 2) {
+            rutaObtenida = obtenerDiaRuta(0);
+            tituloResultado.textContent = `Hoy, ${diasDeLaSemana[dia].toLowerCase} santo : Circulación como día domingo`
+        }
 
         if((fecha == 14) && (mes == 1)) { //Definir feriado previo
             feriadoPrevio = true; 
