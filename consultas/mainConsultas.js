b06620cabe6173984d91ae25c9427e80617d9384
Gonza <gnzx89@gmail.com>
2024-04-05 12:07:50 -0300

    mensaje abonos

--- consultas/mainConsultas.js
+++ consultas/mainConsultas.js
@@ -144,7 +144,7 @@ const definirPosicion = (sel, val, valorSel, pos) => {
     return pos
 }
 
-let displayMensaje = (fecha == 14 && mes == 1 && hora <= 15) ? 'flex' : 'none';
+let displayMensaje = (fecha >= 1 && fecha <= 15 && mes && hora <= 15) ? 'flex' : 'none';
 
 onload = ()=> {
     $('.mensaje-contenedor').css('display' , `${displayMensaje}`);
